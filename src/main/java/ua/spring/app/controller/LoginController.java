f9b7e8f05d579fbc75bdebdf4a71125bb588bd0b
serhii shevchenko <serhii.shevchenko23@gmail.com>
2017-10-04 20:54:03 +0300

    changeLoginController

--- src/main/java/ua/spring/app/controller/LoginController.java
+++ src/main/java/ua/spring/app/controller/LoginController.java
@@ -6,10 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/")
 public class LoginController {
 
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+//    @RequestMapping(value = "/login", method = RequestMethod.GET)
+//    public String login(Model model, String error, String logout) {
+//        if (error != null) model.addAttribute("error", "Username or password incorrect.");
+//        if (logout != null) model.addAttribute("message", "Logout is successfull.");
+//        return "login_page";
+//    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String login(Model model, String error, String logout) {
         if (error != null) model.addAttribute("error", "Username or password incorrect.");
         if (logout != null) model.addAttribute("message", "Logout is successfull.");
